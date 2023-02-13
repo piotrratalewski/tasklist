fe96f322667d471cb88df881ae321c5da1d59eb9
Piotr Ratalewski <piotr.ratalewski@gmail.com>
2023-02-13 19:25:28 +0100

    new repository

---
+++ script/tasklist.js
@@ -0,0 +1,25 @@
+{
+	const tasks = [
+		{
+			content: "zrobić zakupy",
+			done: false,
+		},
+		{
+			content: "wydać pieniądze",
+			done: true,
+		},
+	];
+	const render = () => {
+		let htmlStrin = "";
+		for (const task of tasks) {
+			htmlStrin += `
+    <li>
+    ${task.content}
+    </li>
+    `;
+		}
+		document.querySelector(".js-tasks");
+	};
+	const init = () => {};
+	init();
+}
