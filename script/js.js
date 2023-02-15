caae599663e4fd324d270875f201907b7cd38a85
Piotr Ratalewski <piotr.ratalewski@gmail.com>
2023-02-15 19:27:35 +0100

    add removing button

--- script/js.js
+++ script/js.js
@@ -9,19 +9,37 @@
 			done: true,
 		},
 	];
+	const addNewTask = newTaskContent => {
+		tasks.push({
+			content: newTaskContent,
+		});
+		render();
+	};
+	const removeTask = index => {
+		tasks.splice(index, 1);
+		render();
+	};
 	const render = () => {
 		let htmlString = "";
 		for (const task of tasks) {
 			htmlString += `
     <li>
 	${task.done ? (htmlString.innerHTML = "ok!") : ""}
-	
+	<button class="js-remove">usuń</button>
     ${task.content}
     </li>
     `;
 		}
 
 		document.querySelector(".js-tasks").innerHTML = htmlString;
+
+		const removeButtons = document.querySelectorAll(".js-remove");
+
+		removeButtons.forEach((removeButton, index) => {
+			removeButton.addEventListener("click", () => {
+				removeTask(index);
+			});
+		});
 	};
 	const onFormSubmit = event => {
 		event.preventDefault();
@@ -30,12 +48,6 @@
 			return;
 		}
 		addNewTask(newTaskContent);
-	};
-	const addNewTask = newTaskContent => {
-		tasks.push({
-			content: newTaskContent,
-		});
-		render();
 	};
 
 	const init = () => {
