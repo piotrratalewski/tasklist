ee7a4a9fedc63e06fafd11ff7a5093849fed100f
Piotr Ratalewski <piotr.ratalewski@gmail.com>
2023-02-15 19:49:37 +0100

    binding evenst functions

--- script/js.js
+++ script/js.js
@@ -24,6 +24,23 @@
 		tasks[taskIndex].done = !tasks[taskIndex].done;
 		render();
 	};
+	const bindEvents = () => {
+		const removeButtons = document.querySelectorAll(".js-remove");
+
+		removeButtons.forEach((removeButton, index) => {
+			removeButton.addEventListener("click", () => {
+				removeTask(index);
+			});
+		});
+
+		const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+		toggleDoneButtons.forEach((toggleDoneButton, index) => {
+			toggleDoneButton.addEventListener("click", () => {
+				toggleTaskDone(index);
+			});
+		});
+	};
 
 	const render = () => {
 		let htmlString = "";
@@ -40,22 +57,7 @@
 		}
 
 		document.querySelector(".js-tasks").innerHTML = htmlString;
-
-		const removeButtons = document.querySelectorAll(".js-remove");
-
-		removeButtons.forEach((removeButton, index) => {
-			removeButton.addEventListener("click", () => {
-				removeTask(index);
-			});
-		});
-
-		const toggleDoneButtons = document.querySelectorAll(".js-done");
-
-		toggleDoneButtons.forEach((toggleDoneButton, index) => {
-			toggleDoneButton.addEventListener("click", () => {
-				toggleTaskDone(index);
-			});
-		});
+		bindEvents();
 	};
 	const onFormSubmit = event => {
 		event.preventDefault();
