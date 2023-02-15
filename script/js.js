57fc489b75b412e3b793b14140973b602bae5328
Piotr Ratalewski <piotr.ratalewski@gmail.com>
2023-02-15 19:07:39 +0100

    add new tasks

--- script/js.js
+++ script/js.js
@@ -23,14 +23,25 @@
 
 		document.querySelector(".js-tasks").innerHTML = htmlString;
 	};
+	const onFormSubmit = event => {
+		event.preventDefault();
+		const newTaskContent = document.querySelector(".js-newTask").value.trim();
+		if (newTaskContent === "") {
+			return;
+		}
+		addNewTask(newTaskContent);
+	};
+	const addNewTask = newTaskContent => {
+		tasks.push({
+			content: newTaskContent,
+		});
+		render();
+	};
+
 	const init = () => {
 		render();
 		const form = document.querySelector(".js-form");
-		form.addEventListener("submit", event => {
-			event.preventDefault();
-			const newTaskContent = document.querySelector(".js-newTask").value.trim();
-			console.log(newTaskContent);
-		});
+		form.addEventListener("submit", onFormSubmit);
 	};
 	init();
 }
