2f4123f0daf67590a7140b284b9360a1ea9e9cfe
Piotr Ratalewski <piotr.ratalewski@gmail.com>
2023-02-15 19:43:57 +0100

    ad toggle button

--- script/js.js
+++ script/js.js
@@ -19,12 +19,20 @@
 		tasks.splice(index, 1);
 		render();
 	};
+
+	const toggleTaskDone = taskIndex => {
+		tasks[taskIndex].done = !tasks[taskIndex].done;
+		render();
+	};
+
 	const render = () => {
 		let htmlString = "";
 		for (const task of tasks) {
 			htmlString += `
-    <li>
-	${task.done ? (htmlString.innerHTML = "ok!") : ""}
+    <li
+	${task.done ? ' style="text-decoration: line-through"' : ""}
+	>
+	<button class="js-done">zrobione?</button>
 	<button class="js-remove">usuń</button>
     ${task.content}
     </li>
@@ -38,6 +46,14 @@
 		removeButtons.forEach((removeButton, index) => {
 			removeButton.addEventListener("click", () => {
 				removeTask(index);
+			});
+		});
+
+		const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+		toggleDoneButtons.forEach((toggleDoneButton, index) => {
+			toggleDoneButton.addEventListener("click", () => {
+				toggleTaskDone(index);
 			});
 		});
 	};
